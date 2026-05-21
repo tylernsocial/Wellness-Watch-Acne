6147f7be6178839001684cb815d8169b5d760f16
tylernsocial <tylernsocial@gmail.com>
2026-05-20 21:01:31 -0600

    Enhance image preprocessing function to handle invalid images and return normalized array

--- Backend/predict.py
+++ Backend/predict.py
@@ -7,7 +7,7 @@
 import json ## loads class_names.json
 import numpy as np ## converts images into an array the model can understand
 import tensorflow as tf ## loads final_model.keras and runs predictions
-from PIL import Image ## used for opening and resizing uploaded images 
+from PIL import Image, UnidentifiedImageError ## used for opening and resizing uploaded images 
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent # find the folder where predict.py is located.
@@ -23,16 +23,18 @@ def preprocess_image(image_file):
     '''
     prepares an uploaded image so it matches the format expected by the model
     '''
+    try:
+        img = Image.open(image_file)
+        rgb_img = img.convert('RGB')
+        resized_img = rgb_img.resize((224, 224))
 
-    img = Image.open(image_file)
-    rgb_img = img.convert('RGB')
-    resized_img = rgb_img.resize((224, 224))
-
-    img_array = np.array(resized_img).astype("float32")
-    img_array = img_array / 255.0 ## normalize pixel values from 0-255 to 0-1 since cnn works better on smaller values
-    img_array = np.expand_dims(img_array, axis=0) ## needs to change shape to (x,y,z) to (n,x,y,z) since that is what the model requires since images were sent it batches, (1 image, 224 height, 224 width, 3 colour channels)
-    
-    return img_array
+        img_array = np.array(resized_img).astype("float32")
+        img_array = img_array / 255.0 ## normalize pixel values from 0-255 to 0-1 since cnn works better on smaller values
+        img_array = np.expand_dims(img_array, axis=0) ## needs to change shape to (x,y,z) to (n,x,y,z) since that is what the model requires since images were sent it batches, (1 image, 224 height, 224 width, 3 colour channels)
+        
+        return img_array
+    except UnidentifiedImageError:
+        raise ValueError("Uploaded file is not a valid image.")
 
 def predict_acne_class(image_file):
     '''
@@ -50,3 +52,6 @@ def predict_acne_class(image_file):
         "confidence": round(confidence * 100, 2)
     }
 
+if __name__ == "__main__":
+    result = predict_acne_class("test.jpg")
+    print(result)
