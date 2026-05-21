81cb5003741a7319d0add37f8e0e0cdd12077464
tylernsocial <tylernsocial@gmail.com>
2026-05-20 20:55:31 -0600

    Refactor image preprocessing and prediction functions to return structured output

--- Backend/predict.py
+++ Backend/predict.py
@@ -23,16 +23,30 @@ def preprocess_image(image_file):
     '''
     prepares an uploaded image so it matches the format expected by the model
     '''
+
     img = Image.open(image_file)
-
     rgb_img = img.convert('RGB')
+    resized_img = rgb_img.resize((224, 224))
 
-    resized_img = rgb_img.img.resize((224, 224))
-
-    img_array = np.array(resized_img)
-
+    img_array = np.array(resized_img).astype("float32")
     img_array = img_array / 255.0 ## normalize pixel values from 0-255 to 0-1 since cnn works better on smaller values
-
-    img_array = np.expand_dims(img_array, axis=0)
-
+    img_array = np.expand_dims(img_array, axis=0) ## needs to change shape to (x,y,z) to (n,x,y,z) since that is what the model requires since images were sent it batches, (1 image, 224 height, 224 width, 3 colour channels)
+    
     return img_array
+
+def predict_acne_class(image_file):
+    '''
+    runs the model on an image and returns the predicted class and confidence 
+    '''
+
+    processed_img = preprocess_image(image_file)
+    predictions = model.predict(processed_img) ## model sends back a list of prediction scores ie:[[0.05, 0.10, 0.80, 0.05]]
+    predicted_index = int(np.argmax(predictions[0])) ## finds the index with the largest prediction score predictions[0] = [0.05, 0.10, 0.80, 0.05] since batches
+    confidence = float(np.max(predictions[0])) ## get the largest prediction score
+    predicted_class = class_names[predicted_index] ## get the class based on the found index with the largest predicted score
+
+    return {
+        "prediction": predicted_class,
+        "confidence": round(confidence * 100, 2)
+    }
+
