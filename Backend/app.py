c6a546bd900ed73540aaea0d3b2e232c8bcc2466
tylernsocial <tylernsocial@gmail.com>
2026-05-21 21:39:17 -0600

    Add overview comments to app.py outlining the application flow

---
+++ Backend/app.py
@@ -0,0 +1,13 @@
+## goal of app.py:
+# overview:
+# react frontend
+# User uploads image
+        #↓
+# Flask backend receives image
+        #↓
+# predict.py preprocesses image and runs model
+        #↓
+# Flask backend sends prediction back
+        #↓
+# React frontend displays result
+
