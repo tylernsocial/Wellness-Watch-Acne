0ab44d2b7c3b2b264757b08c2e08f951ffa41ca4
tylernsocial <tylernsocial@gmail.com>
2026-05-21 22:08:36 -0600

    Implemented and tested allowed_file function

--- Backend/app.py
+++ Backend/app.py
@@ -18,3 +18,24 @@
 # will also need the import CORS since react will use a different port than Flask,
 # and by default browsers may block some requests between different origins for security reasons
 # CORS(app) allows for the React frontend to call this backend
+
+from flask import Flask, request, jsonify 
+from flask_cors import CORS 
+from predict import predict_acne_class
+from pathlib import Path
+
+app = Flask(__name__)
+CORS(app)
+
+EXTENSIONS = {"png", "jpg", "jpeg", "webp"}
+
+def allowed_file(filename):
+    ext = Path(filename).suffix.lower().replace(".", "") ## gets .JPG -> jpg
+    return ext in EXTENSIONS
+
+# ext1 = allowed_file("skin.jpg")
+# ext2 = allowed_file("skin.PNG")
+# ext3 = allowed_file("data.csv")
+# ext4 = allowed_file("bomberman")
+
+# print(ext1, ext2, ext3, ext4)
